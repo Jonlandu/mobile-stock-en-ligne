d9057654efdb66cc84634fa48b075e37206b187a
Djidji_kind <kindombeav24@gmail.com>
2023-05-24 16:15:17 +0100

    mobile, definition des requetes dans le fichier requetes.php

--- lib/utils/Requetes.dart
+++ lib/utils/Requetes.dart
@@ -1,38 +1,53 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'Constantes.dart';
-
 class HttpResponse {
   bool status;
   Map? data;
   String? errorMsg;
   bool? isException;
-
   HttpResponse(
       {this.data, required this.status, this.errorMsg, this.isException});
 }
 
-Future<HttpResponse> postData(String api_url, Map data) async {
+Future<dynamic> getData(String url_api, {String? token}) async {
   try {
-    var url = Uri.parse("${Constantes.BASE_URL}$api_url");
-    String dataStr = json.encode(data);
-    var response =
-    await http.post(url, body: dataStr, headers: {"Content-Type":"application/json"}).timeout(Duration(seconds: 5));
-    var successList = [200, 201];
-    var msg = json.decode(response.body);
-    var st = successList.contains(response.statusCode);
-
-    return HttpResponse(status: st, data: msg); // {"status": st, "msg": msg};
-    // return null;
+    var url = Uri.parse("${Constantes.BASE_URLS}$url_api");
+    var reponse = await http.get(url, headers: {
+      "Authorization": "Bearer ${token ?? Constantes.DefaultToken}"
+    }); //.timeout(Duration(seconds: 5));
+    print(reponse.runtimeType);
+    print(reponse.body.runtimeType);
+    print(reponse.body);
+    print(reponse.statusCode);
+    if (reponse.statusCode == 200) {
+      return json.decode(reponse.body);
+    }
+    return null;
   } catch (e, trace) {
+    print(e.toString());
+    print(trace.toString());
+    return null;
+  }
+}
+Future<HttpResponse> postData(String api_url, Map data) async{
+  try{
+    var url=Uri.parse("${Constantes.BASE_URLS}$api_url");
+    String dataStr=json.encode(data);
+    var response=await http.post(url, body: dataStr, headers: {"Content-Type":"application/json"}).timeout(Duration(seconds: 5)) ;
+    var successList=[200, 201];
+    var msg=json.decode(response.body);
+    var st=successList.contains(response.statusCode);
+    if(response.statusCode==500) throw Exception(msg);
+    return  HttpResponse(status: st, data: msg);// {"status": st, "msg": msg};
+    // return null;
+  }catch(e, trace){
     print(e.toString());
     print(trace.toString());
     // return null;
     //return {"status": false, "error_msg": };
-    return HttpResponse(
-        status: false,
-        errorMsg: "Erreur inattendue",
-        isException: true); // {"status": st, "msg": msg};
+    return  HttpResponse(status: false, errorMsg: "Erreur inattendue, Problème de connexion", isException: true);// {"status": st, "msg": msg};
+
   }
 }
 
