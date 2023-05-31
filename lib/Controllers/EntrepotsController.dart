c971eff308563fef3d70cc3578f318467ea2339a
Elvira <mvg.elvira13@gmail.com>
2023-05-31 11:42:22 +0100

    Mise a jour EntrepotController

--- lib/Controllers/EntrepotsController.dart
+++ lib/Controllers/EntrepotsController.dart
@@ -14,35 +14,28 @@ class EntrepotCtrl with ChangeNotifier {
 
   void recuperDataEntrepot() async{
     var url = "${Endpoints.entrepotEndpoint}";
-    print(url);
-
     String? token=stockage?.read(StockageKeys.userToken);
+
     notifyListeners();
 
     var reponse = await getData(url);
     if(reponse != null){
-      print(reponse);
       List<EntrepotModele> tempo = reponse.map<EntrepotModele>((e) => EntrepotModele.fromJson(e)).toList();
       entrepots = tempo;
 
       notifyListeners();
-
-      print("niveau controlleur");
     }
   }
 
   Future<bool> envoieDataEntrepot(Map data) async{
     var url = "${Endpoints.entrepotEndpoint}";
-    print(url);
-
     var reponse = await postData(url, data);
-
     if(reponse.status){
-      print(reponse.data);
-      List<EntrepotModele> tempo = [reponse.data].map<EntrepotModele>((e) => EntrepotModele.fromJson(e!)).toList();
-      entrepots.add(tempo as EntrepotModele) ;
+      var tempo = EntrepotModele.fromJson(reponse.data?["entrepot"]);
+      entrepots.add(tempo);
 
       notifyListeners();
+
       return true;
     }
     return false;
