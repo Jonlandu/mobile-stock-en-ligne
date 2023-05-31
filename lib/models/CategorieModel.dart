456a1fd60ad0ba25b3bcceb91a8dfdabf7176273
Djidji_kind <kindombeav24@gmail.com>
2023-05-31 17:11:37 +0100

    creation du modele categorie

--- lib/models/CategorieModel.dart
+++ lib/models/CategorieModel.dart
@@ -9,29 +9,33 @@ CategorieModel categorieModelFromJson(String str) => CategorieModel.fromJson(jso
 String categorieModelToJson(CategorieModel data) => json.encode(data.toJson());
 
 class CategorieModel {
-
+  int? id;
   String? designation;
   String? description;
+  int? entrepot_id;
 
 
   CategorieModel({
-
+    this.id,
     this.designation,
     this.description,
+    this.entrepot_id
 
   });
 
   factory CategorieModel.fromJson(Map json) => CategorieModel(
-
+    id: json["id"],
     designation: json["designation"],
     description: json["description"],
+    entrepot_id: json["entrepot_id"]
 
   );
 
   Map<String, dynamic> toJson() => {
-
+    "id":id,
     "designation": designation,
     "description": description,
+    "entrepot_id": entrepot_id
 
   };
 }
