73b4f04a52f6efa5eff27b1c178cb69af8e2751c
Elvira <mvg.elvira13@gmail.com>
2023-05-31 11:41:57 +0100

    Mise a jour EntrepotModele

--- lib/Model/EntrepotModele.dart
+++ lib/Model/EntrepotModele.dart
@@ -12,22 +12,26 @@ class EntrepotModele {
   String? nom;
   int? userId;
   DateTime? createdAt;
+  DateTime? updatedAt;
 
   EntrepotModele({
     this.nom,
     this.userId,
     this.createdAt,
+    this.updatedAt,
   });
 
   factory EntrepotModele.fromJson(Map json) => EntrepotModele(
     nom: json["nom"],
     userId: json["user_id"],
     createdAt: json["created_at"] == null ? null : DateTime.parse(json["created_at"]),
+    updatedAt: json["updated_at"] == null ? null : DateTime.parse(json["updated_at"]),
   );
 
   Map<String, dynamic> toJson() => {
     "nom": nom,
     "user_id": userId,
     "created_at": createdAt?.toIso8601String(),
+    "updated_at": updatedAt?.toIso8601String(),
   };
 }
