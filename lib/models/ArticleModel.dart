43e24870465f2ef5a2b994ea5f431a5bca9967f9
Djidji_kind <kindombeav24@gmail.com>
2023-05-31 17:14:14 +0100

    creation modele article

--- lib/models/ArticleModel.dart
+++ lib/models/ArticleModel.dart
@@ -9,41 +9,44 @@ ArticleModel articleModelFromJson(String str) => ArticleModel.fromJson(json.deco
 String articleModelToJson(ArticleModel data) => json.encode(data.toJson());
 
 class ArticleModel {
+  int? id;
   String? nomArticle;
   String? descriptionArticle;
   String? unite;
   int? stockMinimal;
   int? stockInitial;
   int? categorieId;
-  int? entrepotId;
 
   ArticleModel({
+    this.id,
     this.nomArticle,
     this.descriptionArticle,
     this.unite,
     this.stockMinimal,
     this.stockInitial,
     this.categorieId,
-    this.entrepotId,
+
   });
 
   factory ArticleModel.fromJson(Map json) => ArticleModel(
+    id: json["id"],
     nomArticle: json["nom_article"],
     descriptionArticle: json["description_article"],
     unite: json["unite"],
     stockMinimal: json["stock_minimal"],
     stockInitial: json["stock_initial"],
     categorieId: json["categorie_id"],
-    entrepotId: json["entrepot_id"],
+
   );
 
   Map<String, dynamic> toJson() => {
+    "id":id,
     "nom_article": nomArticle,
     "description_article": descriptionArticle,
     "unite": unite,
     "stock_minimal": stockMinimal,
     "stock_initial": stockInitial,
     "categorie_id": categorieId,
-    "entrepot_id": entrepotId,
+
   };
 }
