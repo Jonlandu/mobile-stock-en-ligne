b168f1b9ffb17cc81544bc6c12e0e91918da6254
gradi yalala <gradiyalala@gmail.com>
2023-06-05 16:20:19 +0100

    ensemble des pages et widget regroupant les fonctionnalitées d'affichage de la categorie, des articles et les details sur un seul article

--- lib/utils/RoutesManager.dart
+++ lib/utils/RoutesManager.dart
@@ -1,48 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:squelette_mobile_parcours/pages/CreateArticlePage.dart';
-
-import '../pages/CreerCategoriePage.dart';
-
+import 'package:squelette_mobile_parcours/Model/ArticleModel.dart';
 import 'package:squelette_mobile_parcours/pages/Auth/LoginPage.dart';
 import 'package:squelette_mobile_parcours/pages/Auth/RegisterPage.dart';
-import 'package:squelette_mobile_parcours/pages/DashboardPage.dart';
-import 'package:squelette_mobile_parcours/pages/Entrepot/EntrepotPages.dart';
+import 'package:squelette_mobile_parcours/pages/DetailsArticlePage.dart';
+import 'package:squelette_mobile_parcours/pages/ListArticlePage.dart';
 import 'package:squelette_mobile_parcours/pages/LoadingPage.dart';
 
-import '../pages/Entrepot/CreationEntrepotPage.dart';
+import '../pages/ListCategoriePage.dart';
 import '../pages/TemplatePage.dart';
-
 import 'Routes.dart';
 
 class RoutesManager {
   static Route route(RouteSettings r) {
     switch (r.name) {
       case Routes.TemplateRoutes:
-        //var args=r.arguments as String?;
-        return MaterialPageRoute(builder: (_) => CategoriePage());
 
-        //var args=r.arguments as String?;
+
         return MaterialPageRoute(builder: (_) => TemplatePage());
       case Routes.LoadingRoutes:
         return MaterialPageRoute(builder: (_) => LoadingPage());
       case Routes.LoginRoute:
         return MaterialPageRoute(builder: (_) => LoginPage());
       case Routes.RegisterRoute:
-        return MaterialPageRoute(builder: (_) => RegisterPage());
-      case Routes.CategorieRoute:
-      return MaterialPageRoute(builder: (_) => CategoriePage());
-      case Routes.ArticlesRoute:
-        return MaterialPageRoute(builder: (_) => ArticlePage());
         return MaterialPageRoute(builder: (_)=> RegisterPage());
-      case Routes.DashboardRoute:
-        return MaterialPageRoute(builder: (_) => DashboardPage());
-      case Routes.EntrepotRoute:
-        return MaterialPageRoute(builder: (_)=> EntrepotPage());
-      case Routes.CreationEntrepotRoute:
-        return MaterialPageRoute(builder: (_)=> CreationEntrepotPage());
+      case Routes.ListCategorieRoute:
+        return MaterialPageRoute(builder: (_)=> ListCategoriePage());
+      case Routes.ListArticleRoute:
+        return MaterialPageRoute(builder: (_)=> ListArticlePage());
+      case Routes.DetailsArticleRoute:
+        var args=r.arguments as Map?;
+        var elt ={};
+        var articleData=ArticleModel.fromJson(args ?? elt);
 
-        default:
-        return MaterialPageRoute(builder: (_) => CategoriePage());
+        return MaterialPageRoute(builder: (_)=> DetailsArticlePage(article: articleData));
+
+
+      default:
+        return MaterialPageRoute(builder: (_) => TemplatePage());
     }
   }
 }
