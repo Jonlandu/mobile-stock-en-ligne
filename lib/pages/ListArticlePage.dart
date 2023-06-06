8e193fe90a14ab928a8f8da28bdef62ebf84d069
gradi yalala <gradiyalala@gmail.com>
2023-06-06 16:19:59 +0100

    fix: design liste article Page

--- lib/pages/ListArticlePage.dart
+++ lib/pages/ListArticlePage.dart
@@ -40,6 +40,7 @@ class _ListArticlePage extends State<ListArticlePage> {
 
   AppBar _appBar() {
     return AppBar(
+
       leading: SizedBox(),
       leadingWidth: 0,
       title: Container(height: 55, child: Image.asset("assets/orange.jpg"),),
@@ -73,59 +74,82 @@ class _ListArticlePage extends State<ListArticlePage> {
       crossAxisAlignment: CrossAxisAlignment.start,
 
       children: [
+
         Padding(
           padding: const EdgeInsets.all(5.0),
           child: Container(child: Text("Nombre des articles: ${articlectrl.articledataList.length}",
               style:TextStyle(color: Colors.orange,fontSize: 15))),
         ),
         SingleChildScrollView(
+
           child: ListView.builder(
+
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
               itemCount: articlectrl.articledataList.length,
               itemBuilder: (ctx,i){
                 var articleconvert = articlectrl.articledataList[i];
-                return Container(
-
-                  child: TextButton(
-                    child: ListTile(
-                      leading: Icon(Icons.shopping_cart),
-                      trailing: Icon(Icons.arrow_forward_ios_outlined),
-                      title: Row(
-                        children: [
-                          Column(
-                            children: [
-                              Divider(
-                                height: 1,
-                                color: Colors.red,
-                              ),
-                              Text(
-                                "${articleconvert.nomArticle}  "
-                                    "${articleconvert.stockMinimal}  "
-                                    "${articleconvert.unite}  ",
-                                style: TextStyle(color: Colors.black, fontSize: 20,),),
-                              Divider(
-                                height: 1,
-                                color: Colors.red,
-                              ),],
+                return Column(
+                  children: [SizedBox(
+                    height: 20,
+                  ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            //topRight: Radius.circular(10),
+                            //bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10)
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
                           ),
                         ],
                       ),
+                      child:
+
+                      TextButton(
+                        child: ListTile(
+                          leading:Image.asset("assets/flag.jpg"),
+                          trailing: Icon(Icons.arrow_forward_ios_outlined),
+                          title: Row(
+                            children: [
+                              Column(
+
+                                children: [
+                                  Text(
+                                    "${articleconvert.nomArticle}  "
+                                        "${articleconvert.stockMinimal}  "
+                                        "${articleconvert.unite}  ",
+                                    style: TextStyle(
+                                      color: Colors.black, fontSize: 20,
+                                    ),
+                                  ),],
+                              ),
+                            ],
+                          ),
 
 
-                      //trailing: IconButton(onPressed:(){
-                      //Navigator.push(context, MaterialPageRoute(builder: (_)=>FormFidelePage(fidele_id: f.id,)));
-                      //}, icon:Icon(Icons.arrow_circle_right)),
-                      //leading: f.image != null? Image.network("${Constantes.BASE_URL}/${(f.image!)}"):Icon(Icons.error),
+                          //trailing: IconButton(onPressed:(){
+                          //Navigator.push(context, MaterialPageRoute(builder: (_)=>FormFidelePage(fidele_id: f.id,)));
+                          //}, icon:Icon(Icons.arrow_circle_right)),
+                          //leading: f.image != null? Image.network("${Constantes.BASE_URL}/${(f.image!)}"):Icon(Icons.error),
+                        ),
+                        onPressed: (){
+                          Navigator.pushNamed(context, Routes.DetailsArticleRoute,
+                              arguments:articleconvert.toJson()
+                          );
+
+
+                        },
+                      ),
                     ),
-                    onPressed: (){
-                      Navigator.pushNamed(context, Routes.DetailsArticleRoute,
-                          arguments:articleconvert.toJson()
-                      );
-
-
-                    },
-                  ),
+                  ],
                 );
               }),
         ),
