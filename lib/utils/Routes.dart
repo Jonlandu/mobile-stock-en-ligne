71456e5aaa1e9d671f958650216af700f2d2072d
Elvira <mvg.elvira13@gmail.com>
2023-06-07 12:20:27 +0100

    ajout route creation Mouvement

--- lib/utils/Routes.dart
+++ lib/utils/Routes.dart
@@ -12,6 +12,8 @@ class Routes {
   static const String ListArticleRoute= "/listArticle";
   static const String ListCategorieRoute= "/listCategorie";
   static const String DetailsArticleRoute= "/DetailsArticle";
+  static const String CreationMovementRoute= "/creationMouvement";
+
 
 
 }
