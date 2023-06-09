210485ebdfb1268709d8b56469f8b5728f4a0947
Djidji_kind <kindombeav24@gmail.com>
2023-06-09 16:16:25 +0100

    modification du modele article et categorie

--- lib/Model/ArticleModel.dart
+++ lib/Model/ArticleModel.dart
@@ -15,7 +15,8 @@ class ArticleModel {
   String? unite;
   int? stockMinimal;
   int? stockInitial;
-  int? categorieId;
+  int? entrepot_id;
+  int? qte_stock;
 
   ArticleModel({
     this.id,
@@ -24,7 +25,8 @@ class ArticleModel {
     this.unite,
     this.stockMinimal,
     this.stockInitial,
-    this.categorieId,
+    this.entrepot_id,
+    this.qte_stock
 
   });
 
@@ -35,7 +37,8 @@ class ArticleModel {
     unite: json["unite"],
     stockMinimal: json["stock_minimal"],
     stockInitial: json["stock_initial"],
-    categorieId: json["categorie_id"],
+    entrepot_id: json["categorie_id"],
+    qte_stock: json["qte_stock"]
 
   );
 
@@ -46,7 +49,8 @@ class ArticleModel {
     "unite": unite,
     "stock_minimal": stockMinimal,
     "stock_initial": stockInitial,
-    "categorie_id": categorieId,
+    "entrepot_id": entrepot_id,
+    "qte_stock" : qte_stock
 
   };
 }
