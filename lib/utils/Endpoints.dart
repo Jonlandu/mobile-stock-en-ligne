13d1e6fb719cb2a4d87121271189f5e64cffcbc6
Elvira <mvg.elvira13@gmail.com>
2023-06-28 15:58:22 +0100

    ajout endpoints de mouvements et articles recent

--- lib/utils/Endpoints.dart
+++ lib/utils/Endpoints.dart
@@ -1,18 +1,22 @@
 class Endpoints {
-  static const String showCategoriesEndpoints = "/api/categories";
-  static const String createCategorieEndpoints = "/api/categories/create";
   static const String logoutEndpoint = "/api/auth/logout";
   static const String loginEndpoint = "/api/auth/login";
   static const String registerEndpoint = "/api/auth/register";
-  static const String compteArticle = "/api/compteArticle/";
+
+  static const String showCategoriesEndpoints = "/api/categories";
+  static const String createCategorieEndpoints = "/api/categories/create";
   static const String compteCategorie = "/api/compteCategorie/";
 
+  static const String compteArticle = "/api/compteArticle/";
   static const String createArticlesEndpoints = "/api/articles/create";
   static const String showArticlesEndpoints = "/api/articles";
+  static const String articlesRecentEndpoint = "/api/article_recent";
 
   static const String entrepotEndpoint = "/api/entrepots";
 
   static const String typeMouvementEndpoint = "/api/type";
   static const String mouvementEndpoint = "/api/mouvement";
+  static const String mouvementsRecentEndpoint = "/api/mouvement_recent";
+
 
 }
